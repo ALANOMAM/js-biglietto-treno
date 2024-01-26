60ed961bc746d1d7cc649e93edbd0a9bf1d083ba
alan_omam <omamalan@hotmail.it>
2024-01-26 15:31:01 +0100

    impostazione della logica degli sconti, con esposizione risultati in console

--- index.js
+++ index.js
@@ -7,11 +7,28 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 -va applicato uno sconto del 20% per i minorenni
 -va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
 Bonus:
 effettuare dei controlli per capire se l'utente ha inserito correttamente il numero di km e l'età (ovvero se sono effettivamente dei numeri e non testo a caso)
 
 */
 
 
- let age = prompt("Inserire l'età");
- let distance = prompt("inserire il numero di km")
+ /*let age = prompt("Inserire l'età");
+ let distance = prompt("inserire il numero di km");*/
+  let age = 30;
+  let distance = 200;
+ let fullPrice = 0.21 * distance;
+ let price;
+
+ if(age < 18){
+     price = fullPrice - ((20/100) * fullPrice);
+     console.log(price);
+ } else if(age > 65){
+    price = fullPrice - ((40/100) * fullPrice);
+    console.log(price);
+    
+ } else{
+   price = fullPrice;
+    console.log(price);
+ }
