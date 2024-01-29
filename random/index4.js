2ee932f7a8441c5fbb926e6a4d94dd8b35b58409
alan_omam <omamalan@hotmail.it>
2024-01-29 17:10:16 +0100

    sistemato la logica però nella console, ora la trasferisco nella pagina

--- random/index4.js
+++ random/index4.js
@@ -2,15 +2,42 @@
 Scrivi un programma che simula il gioco di "Carta, forbice, sasso". Chiedi all'utente di fare una scelta e confrontala con una scelta casuale del computer.
  Determina il vincitore o se c'è un pareggio 
 */ 
-
+let objectPrompt = "sasso";
 let randomNumber = Math.floor( Math.random()*10);
-
+let object;
 
 if(randomNumber >= 0 && randomNumber<=3 ){
    console.log("sasso",randomNumber)
-} else if(randomNumber >3 && randomNumber <= 6){
+   object = "sasso";
+   if(objectPrompt="sasso"){
+     console.log("hai pereggiato")
+   }else if(objectPrompt="carta"){
+     console.log("hai vinto")
+   }else{
+    console.log("hai perso")
+   }
+
+}else if(randomNumber >3 && randomNumber <= 6){
     console.log("carta",randomNumber)
+    object = "carta";
+    if(objectPrompt="sasso"){
+        console.log("hai perso")
+      }else if(objectPrompt="carta"){
+        console.log("hai pareggiato")
+      }else{
+       console.log("hai vinto")
+      }
+
+
 } else {
     console.log("forbici",randomNumber)
+    object = "forbici";
+    if(objectPrompt="sasso"){
+        console.log("hai vinto")
+      }else if(objectPrompt="carta"){
+        console.log("hai perso")
+      }else{
+       console.log("hai pareggiato")
+      }
 }
 
