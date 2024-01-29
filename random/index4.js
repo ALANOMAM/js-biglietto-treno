1da88ef21cb4846ec42349bb6e549eb90a403690
alan_omam <omamalan@hotmail.it>
2024-01-29 17:45:33 +0100

    PROGRAMMA QUASI FINITO(sistemato stile devo solo riguardare alcuni elementi della logica perchè non torna sempre )

--- random/index4.js
+++ random/index4.js
@@ -2,56 +2,50 @@
 Scrivi un programma che simula il gioco di "Carta, forbice, sasso". Chiedi all'utente di fare una scelta e confrontala con una scelta casuale del computer.
  Determina il vincitore o se c'è un pareggio 
 */ 
-let objectPrompt = "sasso";
+let objectPrompt = prompt("Scegli sasso, carta o forbici ?");
 let randomNumber = Math.floor( Math.random()*10);
 let object;
 
 document.getElementById("userObject").innerHTML=objectPrompt;
 
 if(randomNumber >= 0 && randomNumber<=3 ){
-   console.log("sasso",randomNumber)
    object = "sasso";
    document.getElementById("computerObject").innerHTML =object;
    if(objectPrompt="sasso"){
-     console.log("hai pereggiato")
      document.getElementById("result").innerHTML = "hai pereggiato"
    }else if(objectPrompt="carta"){
-     console.log("hai vinto")
      document.getElementById("result").innerHTML = "hai vinto"
-   }else{
-    console.log("hai perso")
+   }else if(objectPrompt="forbici"){
     document.getElementById("result").innerHTML = "hai perso"
+   } else{
+    document.getElementById("result").innerHTML = "input non valido"
    }
 
 }else if(randomNumber >3 && randomNumber <= 6){
-    console.log("carta",randomNumber)
     object = "carta";
     document.getElementById("computerObject").innerHTML =object;
     if(objectPrompt="sasso"){
-        console.log("hai perso")
         document.getElementById("result").innerHTML = "hai perso"
       }else if(objectPrompt="carta"){
-        console.log("hai pareggiato")
         document.getElementById("result").innerHTML = "hai pereggiato"
-      }else{
-       console.log("hai vinto")
+      }else if(objectPrompt="forbici"){
        document.getElementById("result").innerHTML = "hai vinto"
+      }else{
+        document.getElementById("result").innerHTML = "input non valido"
       }
 
 
 } else {
-    console.log("forbici",randomNumber)
     object = "forbici";
     document.getElementById("computerObject").innerHTML =object;
     if(objectPrompt="sasso"){
-        console.log("hai vinto")
         document.getElementById("result").innerHTML = "hai vinto"
       }else if(objectPrompt="carta"){
-        console.log("hai perso")
         document.getElementById("result").innerHTML = "hai perso"
-      }else{
-       console.log("hai pareggiato")
+      }else if(objectPrompt="forbici"){
        document.getElementById("result").innerHTML = "hai pereggiato"
+      }else{
+        document.getElementById("result").innerHTML = "input non valido"
       }
 }
 
