1d68744629acf6e63d0133a6edae81aa63a168be
alan_omam <omamalan@hotmail.it>
2024-01-29 15:56:46 +0100

    FINE PROGRAMMA MEDIA

--- media/index3.js
+++ media/index3.js
@@ -3,15 +3,15 @@ Scrivi un programma che prende tre numeri in input usando prompt.
 Calcola la media di questi numeri e visualizza il risultato con almeno due decimali.
 */
 
-/*let firstNumber = prompt("Inserisci il primo numero : ");
+let firstNumber = prompt("Inserisci il primo numero : ");
 let secondNumber = prompt("Inserisci il secondo numero : ");
-let thirdNumber =  prompt("Inserisci il terzo numero : ");*/
+let thirdNumber =  prompt("Inserisci il terzo numero : ");
 
 
-let firstNumber = 90
-let secondNumber =7
-let thirdNumber = 9
+let media = (( Number(firstNumber)+ Number(secondNumber)+ Number(thirdNumber))/3).toFixed(2);
 
-let media = ((firstNumber+secondNumber+thirdNumber)/3).toFixed(2);
+document.getElementById("average").innerHTML = media;
 
-document.getElementById("media").innerHTML = media;
+document.getElementById("num1").innerHTML = firstNumber;
+document.getElementById("num2").innerHTML = secondNumber;
+document.getElementById("num3").innerHTML = thirdNumber;
