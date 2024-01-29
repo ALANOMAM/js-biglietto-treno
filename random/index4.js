c1edcc22d8459f96ed15d19f6782620542c87156
alan_omam <omamalan@hotmail.it>
2024-01-29 16:44:53 +0100

    impostazione cartella sasso-carta-forbici

---
+++ random/index4.js
@@ -0,0 +1,4 @@
+/*
+Scrivi un programma che simula il gioco di "Carta, forbice, sasso". Chiedi all'utente di fare una scelta e confrontala con una scelta casuale del computer.
+ Determina il vincitore o se c'è un pareggio 
+*/ 
