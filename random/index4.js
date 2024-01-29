63345c7df29d73a5790c2a257a62e391325b9703
alan_omam <omamalan@hotmail.it>
2024-01-29 16:57:27 +0100

    Fatto la base della parte logica, ovvero ho associato una seria di numeri random a sasso, carta o forbici

--- random/index4.js
+++ random/index4.js
@@ -2,3 +2,15 @@
 Scrivi un programma che simula il gioco di "Carta, forbice, sasso". Chiedi all'utente di fare una scelta e confrontala con una scelta casuale del computer.
  Determina il vincitore o se c'è un pareggio 
 */ 
+
+let randomNumber = Math.floor( Math.random()*10);
+
+
+if(randomNumber >= 0 && randomNumber<=3 ){
+   console.log("sasso",randomNumber)
+} else if(randomNumber >3 && randomNumber <= 6){
+    console.log("carta",randomNumber)
+} else {
+    console.log("forbici",randomNumber)
+}
+
