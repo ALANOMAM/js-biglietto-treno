e0de3527f5948399009bbbe6a287a14386b77d88
alan_omam <omamalan@hotmail.it>
2024-01-29 14:33:55 +0100

    fatta la lgica di calcolo, ora lavoro su impostare bene il layout aggiungendo anche un po di stile

---
+++ convertitore/index2.js
@@ -0,0 +1,15 @@
+/*Crea un programma che chiede all'utente una temperatura 
+in gradi Celsius e la converte in Fahrenheit, tramite una ricerca su 
+internet potrete trovare la formula corretta per effettuare la conversione. Visualizza la temperatura convertita.*/ 
+
+ /* let tempCelsuis = prompt("Ciao, dammi la temperatura in gradi celsuis");
+
+
+ */
+
+ let tempCelsuis = 45;
+ let tempFahrenheit = tempCelsuis*(9/5)+32;
+ 
+
+ document.getElementById("Celsius").innerHTML = tempCelsuis;
+ document.getElementById("Fahrenheit").innerHTML =tempFahrenheit;
