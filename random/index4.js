dfeea6dacd7bf03a737167ed13a2b779d876afb1
alan_omam <omamalan@hotmail.it>
2024-01-29 17:22:39 +0100

    PARTE LOGICA DEL PROGRAMMA SASSO-CARTA FORBICI FATTO (ora devo solo cancellare i vari console e sitemare lo stile )

--- random/index4.js
+++ random/index4.js
@@ -6,38 +6,54 @@ let objectPrompt = "sasso";
 let randomNumber = Math.floor( Math.random()*10);
 let object;
 
+document.getElementById("userObject").innerHTML=objectPrompt;
+
 if(randomNumber >= 0 && randomNumber<=3 ){
    console.log("sasso",randomNumber)
    object = "sasso";
+   document.getElementById("computerObject").innerHTML =object;
    if(objectPrompt="sasso"){
      console.log("hai pereggiato")
+     document.getElementById("result").innerHTML = "hai pereggiato"
    }else if(objectPrompt="carta"){
      console.log("hai vinto")
+     document.getElementById("result").innerHTML = "hai vinto"
    }else{
     console.log("hai perso")
+    document.getElementById("result").innerHTML = "hai perso"
    }
 
 }else if(randomNumber >3 && randomNumber <= 6){
     console.log("carta",randomNumber)
     object = "carta";
+    document.getElementById("computerObject").innerHTML =object;
     if(objectPrompt="sasso"){
         console.log("hai perso")
+        document.getElementById("result").innerHTML = "hai perso"
       }else if(objectPrompt="carta"){
         console.log("hai pareggiato")
+        document.getElementById("result").innerHTML = "hai pereggiato"
       }else{
        console.log("hai vinto")
+       document.getElementById("result").innerHTML = "hai vinto"
       }
 
 
 } else {
     console.log("forbici",randomNumber)
     object = "forbici";
+    document.getElementById("computerObject").innerHTML =object;
     if(objectPrompt="sasso"){
         console.log("hai vinto")
+        document.getElementById("result").innerHTML = "hai vinto"
       }else if(objectPrompt="carta"){
         console.log("hai perso")
+        document.getElementById("result").innerHTML = "hai perso"
       }else{
        console.log("hai pareggiato")
+       document.getElementById("result").innerHTML = "hai pereggiato"
       }
 }
+
+
 
